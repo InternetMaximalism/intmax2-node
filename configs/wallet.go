11a4b0493b1b709769c8dc188049a7c4c607f65d
signature18632 <intmax18@gmail.com>
2024-08-06 09:54:46 +0700

    Revert "remove WALLET_PRIVATE_KEY_HEX env"
    This reverts commit c28ad905bb2fb2a102c5ddaaa1750e834c02c9ed.

--- configs/wallet.go
+++ configs/wallet.go
@@ -4,4 +4,6 @@ type Wallet struct {
 	MnemonicValue          string `env:"WALLET_MNEMONIC_VALUE"`
 	MnemonicDerivationPath string `env:"WALLET_MNEMONIC_DERIVATION_PATH"`
 	MnemonicPassword       string `env:"WALLET_MNEMONIC_PASSWORD"`
+
+	PrivateKeyHex string `env:"WALLET_PRIVATE_KEY_HEX"`
 }
