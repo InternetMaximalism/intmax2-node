cfac10a1ffdcc728cf7088aa0aebe883958f4c1b
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-08-09 07:03:36 +0200

    Modify balance logs

--- internal/balance_service/errors.go
+++ internal/balance_service/errors.go
@@ -3,16 +3,19 @@ package balance_service
 import "errors"
 
 const (
-	//nolint:gosec
-	ErrTokenTypeRequired                           = "token type is required"
-	ErrETHBalanceCheckArgs                         = "ETH balance check doesn't require additional arguments"
-	ErrERC20BalanceCheckArgs                       = "ERC20 balance check requires a token address"
-	ErrERC721BalanceCheckArgs                      = "ERC721 balance check requires a token address and token ID"
-	ErrERC1155BalanceCheckArgs                     = "ERC1155 balance check requires a token address and token ID"
-	ErrInvalidTokenType                            = "invalid token type. Use 'eth', 'erc20', 'erc721', or 'erc1155'"
-	ErrTokenNotFound                               = "token not found on INTMAX network"
-	ErrFailedToGetBalance                          = "failed to get balance"
+	ErrFailedToGetBalance                          = "failed to get balance on %s network"
 	ErrFetchTokenByTokenAddressAndTokenIDWithDBApp = "Failed to fetch token by tokenAddress and tokenId with DBApp: %v\n"
 )
 
+var ErrInvalidTokenType = errors.New("invalid token type. Use 'eth', 'erc20', 'erc721', or 'erc1155'")
+var ErrTokenNotFound = errors.New("token not found on INTMAX network")
 var ErrFetchBalanceByUserAddressAndTokenInfoWithDBApp = errors.New("failed to fetch balance by user address and token info with DBApp")
+var ErrInvalidPrivateKey = errors.New("invalid private key")
+var ErrRecoverWalletFromPrivateKey = errors.New("fail to recover INTMAX private key")
+var ErrFailedToGetTokenIndex = errors.New("failed to get token index")
+var ErrFailedToGetETHBalance = errors.New("failed to get ETH token balance")
+var ErrFailedToGetERC20Balance = errors.New("failed to get ERC20 token balance")
+var ErrFailedToGetERC721Owner = errors.New("failed to get ERC721 token owner")
+var ErrOwnerNotMatch = errors.New("owner of the token is not the same as the provided address")
+var ErrFailedToGetERC1155Balance = errors.New("failed to get ERC1155 token balance")
+var ErrDepositValidity = errors.New("failed to get deposit validity")
