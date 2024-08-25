8d84d388aadd44c50d8b1c90d6471167a95204dd
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-08-25 05:58:18 +0200

    Fix Merkle tree

--- internal/tree/historical_merkle_tree_test.go
+++ internal/tree/historical_merkle_tree_test.go
@@ -1,6 +1,7 @@
 package tree_test
 
 import (
+	"fmt"
 	"intmax2-node/internal/hash/goldenposeidon"
 	"intmax2-node/internal/tree"
 	"testing"
@@ -11,27 +12,22 @@ import (
 type poseidonHashOut = goldenposeidon.PoseidonHashOut
 
 func TestNewHistoricalPoseidonMerkleTree(t *testing.T) {
-	const cachingSubTreeHeight = 4
+	const cachingSubTreeHeight = 3
 	zeroHash := new(poseidonHashOut).SetZero()
-	mt, err := tree.NewHistoricalPoseidonMerkleTree(32, zeroHash, cachingSubTreeHeight)
+	storage := tree.NewMerkleTreeHistoryOnMemory()
+	mt, err := tree.NewHistoricalPoseidonMerkleTree(32, zeroHash, storage, cachingSubTreeHeight)
 	require.NoError(t, err)
 
-	normalLeaves := []*poseidonHashOut{}
+	numLeaves := 530
+	index := 261
 
-	numLeaves := 260
-	index := 37
-
-	for i := 0; i < numLeaves; i++ {
-		e, err := new(poseidonHashOut).SetRandom()
+	leaves := make([]*tree.PoseidonMerkleLeafWithIndex, numLeaves)
+	for i := 0; i < len(leaves); i++ {
+		leafHash, err := new(poseidonHashOut).SetRandom()
 		require.NoError(t, err)
-		normalLeaves = append(normalLeaves, e)
-	}
-
-	leaves := make([]*tree.PoseidonMerkleLeafWithIndex, len(normalLeaves))
-	for i := 0; i < len(normalLeaves); i++ {
 		leaves[i] = &tree.PoseidonMerkleLeafWithIndex{
 			Index:    i,
-			LeafHash: normalLeaves[i],
+			LeafHash: leafHash,
 		}
 	}
 
@@ -44,6 +40,49 @@ func TestNewHistoricalPoseidonMerkleTree(t *testing.T) {
 	proof, err := mt.Prove(root, index)
 	require.NoError(t, err)
 
+	fmt.Printf("root = %s\n", root)
+	fmt.Printf("leaves[%d] = %v\n", index, leaves[index].LeafHash)
 	err = proof.Verify(root, index, leaves[index].LeafHash)
 	require.NoError(t, err)
+
+	// refresh the tree
+	mt, err = tree.NewHistoricalPoseidonMerkleTree(32, zeroHash, storage, cachingSubTreeHeight)
+	require.NoError(t, err)
+
+	numLeaves = 260
+	index = 261
+
+	leaves2 := make([]*tree.PoseidonMerkleLeafWithIndex, numLeaves)
+	for i := 0; i < len(leaves2); i++ {
+		leafHash, err := new(poseidonHashOut).SetRandom()
+		require.NoError(t, err)
+		leaves2[i] = &tree.PoseidonMerkleLeafWithIndex{
+			Index:    i,
+			LeafHash: leafHash,
+		}
+	}
+
+	_, err = mt.UpdateLeaves(leaves2)
+	require.NoError(t, err)
+
+	proof, err = mt.Prove(root, index)
+	require.NoError(t, err)
+
+	fmt.Printf("root = %s\n", root)
+	fmt.Printf("leaves[%d] = %v\n", index, leaves[index].LeafHash)
+	err = proof.Verify(root, index, leaves[index].LeafHash)
+	require.NoError(t, err)
+
+	root2 := mt.GetRoot()
+	require.NoError(t, err)
+
+	proof2, err := mt.Prove(root2, index)
+	require.NoError(t, err)
+
+	fmt.Printf("root2 = %s\n", root2)
+	fmt.Printf("leaves[%d] = %v\n", index, leaves[index].LeafHash)
+	err = proof2.Verify(root2, index, leaves[index].LeafHash)
+	require.NoError(t, err)
+
+	storage.ReportStats()
 }
