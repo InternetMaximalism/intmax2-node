4dfe723038cc6ddc5f8febae0d710d5cecdd28be
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-08-31 10:51:14 +0200

    Remove hostname (#124)

--- prover/gnark-server/main.go
+++ prover/gnark-server/main.go
@@ -19,22 +19,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	host := os.Getenv("HOST")
-	if host == "" {
-		host = "localhost"
-	}
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
+	fmt.Printf("Preparing server with circuit: %s\n", *circuitName)
 	ctx := handlers.CircuitData(context.InitCircuitData(*circuitName))
 	http.HandleFunc("/health", handlers.HealthHandler)
 	http.HandleFunc("/start-proof", ctx.StartProof)
 	http.HandleFunc("/get-proof", ctx.GetProof)
 	fmt.Printf("Server is running on port %s...", port)
-	addr := fmt.Sprintf("%s:%s", host, port)
+	addr := fmt.Sprintf(":%s", port)
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
