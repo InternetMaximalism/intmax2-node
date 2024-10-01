476cf8c98b07d37134b2a96cdbdee14f910ef6c5
denis468629 <denis@intmax.io>
2024-10-01 22:47:37 +0300

    added extraFee with the fee processing for eth-token (upd)

--- pkg/gas_price_oracle/gas_price_oracle.go
+++ pkg/gas_price_oracle/gas_price_oracle.go
@@ -4,15 +4,17 @@ import (
 	"intmax2-node/configs"
 	"intmax2-node/internal/gas_price_oracle"
 	"intmax2-node/internal/gas_price_oracle/scroll_eth"
+	"intmax2-node/internal/logger"
 )
 
 func NewGasPriceOracle(
 	cfg *configs.Config,
+	log logger.Logger,
 	gpo string,
 	sb ServiceBlockchain,
 ) (GasPriceOracle, error) {
 	if gpo == gas_price_oracle.ScrollEthGPO {
-		return scroll_eth.New(cfg, sb), nil
+		return scroll_eth.New(cfg, log, sb), nil
 	}
 
 	return nil, ErrGPODriverNameInvalid
