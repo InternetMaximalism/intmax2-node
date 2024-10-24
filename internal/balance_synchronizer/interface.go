2a6c56a8602e6f2b4162a434994181a481066f57
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-10-24 22:51:24 +0700

    small fix

--- internal/balance_synchronizer/interface.go
+++ internal/balance_synchronizer/interface.go
@@ -15,7 +15,7 @@ type BlockValidityService interface {
 	BlockContentByTxRoot(txRoot common.Hash) (*block_post_service.PostedBlock, error)
 	GetDepositInfoByHash(depositHash common.Hash) (depositInfo *block_validity_prover.DepositInfo, err error)
 	FetchValidityProverInfo() (*block_validity_prover.ValidityProverInfo, error)
-	FetchUpdateWitness(publicKey *intMaxAcc.PublicKey, currentBlockNumber *uint32, targetBlockNumber uint32, isPrevAccountTree bool) (*block_validity_prover.UpdateWitness, error)
+	FetchUpdateWitness(publicKey *intMaxAcc.PublicKey, currentBlockNumber uint32, targetBlockNumber uint32, isPrevAccountTree bool) (*block_validity_prover.UpdateWitness, error)
 	DepositTreeProof(blockNumber uint32, depositIndex uint32) (*intMaxTree.KeccakMerkleProof, common.Hash, error)
 	BlockTreeProof(rootBlockNumber uint32, leafBlockNumber uint32) (*intMaxTree.PoseidonMerkleProof, error)
 	ValidityPublicInputs(txRoot common.Hash) (validityPublicInputs *block_validity_prover.ValidityPublicInputs, senderLeaves []block_validity_prover.SenderLeaf, err error)
