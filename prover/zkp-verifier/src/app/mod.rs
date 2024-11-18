29b76da287a5a5922c8d6674af38374de6619d20
Baby Bear <132609968+smallbabybear@users.noreply.github.com>
2024-11-19 04:48:15 +0700

    Remove zkp-verifier

--- prover/zkp-verifier/src/app/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod config;
-pub mod encode;
-pub mod error_handler;
-pub mod interface;
-pub mod route;
-pub mod state;
--- prover/zkp-verifier/src/app/route.rs
+++
@@ -1,7 +0,0 @@
-use crate::server::{health, verifier};
-use actix_web::web;
-
-pub fn setup_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service((health::health_check,));
-    cfg.service((verifier::spend::verify_proof,));
-}
